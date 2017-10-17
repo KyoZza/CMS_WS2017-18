53f76588cfaffdcc53dd8b932b6ab1dc62e51c79
Sandro <sandro.scheffmann@gmail.com>
2017-10-17 23:42:32 +0200

    create index file

---
+++ Projekt/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>CMS Projekt WS 2017-18</title>
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <h1>CMS Projekt WS 2017-18</h1>
+            <h3>Let's get it started!</h3>
+        </div>
+    </body>
+</html>
