9c496a1ea3d3e0ea9f16599a7dfbce3b68cbbf25
Trinkwut <s_fabian_89@yahoo.de>
2017-12-12 16:08:50 +0100

    merge conflicts

--- cmsapp/database/seeds/DatabaseSeeder.php
+++ cmsapp/database/seeds/DatabaseSeeder.php
@@ -11,37 +11,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-
          // Role comes before User seeder here.
-       // $this->call(RoleTableSeeder::class);
+        $this->call(RoleTableSeeder::class);
         // User seeder will use the roles above created.
-<<<<<<< HEAD
-       // $this->call(UserTableSeeder::class);
-=======
-        $this->call(UserTableSeeder::class);s
->>>>>>> d76a1dda24c186ae4139c317b8830dc61746b40b
+        $this->call(UserTableSeeder::class);
 
       // $this->call(PageTableSeeder::class);
 //
-        $this->call(NavItemTableSeeder::class); 
+        //$this->call(NavItemTableSeeder::class); 
 
-<<<<<<< HEAD
        //$this->call(ThemeTableSeeder::class);
        // $this->call(ThemeHeaderOptionsTableSeeder::class);
       //  $this->call(HeaderImageTableSeeder::class); 
         
-       // $this->call(ThemeColorTableSeeder::class);
-=======
-        $this->call(NavItemTableSeeder::class); */
 
-        /*$this->call(ThemeTableSeeder::class);
-        $this->call(ThemeHeaderOptionsTableSeeder::class);
-        $this->call(HeaderImageTableSeeder::class);
->>>>>>> d76a1dda24c186ae4139c317b8830dc61746b40b
         
-        $this->call(ThemeColorTableSeeder::class);*/
-        $this->call(FontsTableSeeder::class);
+        //$this->call(ThemeColorTableSeeder::class);
+        //$this->call(FontsTableSeeder::class);
         //Uncomment the seeds you need!!
     }
 }
